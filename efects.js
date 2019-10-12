ceb42db49e27f611ddc04a4d55616ced0b6a581a
anaflaviafoppa <anaffoppa@gmail.com>
2019-10-12 17:40:30 +0100

    V1
    Versão V1 -Publicação 01

--- efects.js
+++ efects.js
@@ -1,3 +1,25 @@
+/*NAV BAR */
+
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#navegacao');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navegacao").css('background-color', '#f0f0f0');
+       } else {
+          $('.navegacao').css('background-color', 'red');
+       }
+   });
+    }
+});
+
+
+/*NAVEGACAO TESTE*/
+
+
 /*SMOTH SCROLL*/
 
 
